b58dbf6ed8f72e577bb83234d0b1d3c8af096496
SHabaj-dev <shabaj@leadmint.io>
2025-10-23 22:45:51 +0530

    Initial Android project setup with Jetpack Compose

---
+++ app/src/main/java/com/sbz/chess/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sbz.chess
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.sbz.chess.ui.theme.ChessTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChessTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                }
+            }
+        }
+    }
+}
